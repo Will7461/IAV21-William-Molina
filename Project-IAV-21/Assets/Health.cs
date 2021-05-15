318f3f544c37749171c483c810e908c78dc4f4df
Will7461 <43720307+Will7461@users.noreply.github.com>
2021-05-15 14:22:08 +0200

    Componente Health y barra de vida en UI

---
+++ Project-IAV-21/Assets/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class Health : MonoBehaviour
+{
+    public LifeSlider lifeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void takeDamage(int dmg)
+    {
+        int life = (int)Variables.Object(gameObject).Get("Life");
+        life -= dmg;
+        Variables.Object(gameObject).Set("Life", life);
+        lifeSlider.UpdateUISlider();
+    }
+}
